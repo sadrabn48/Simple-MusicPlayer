d44cb13ea1b66f2f0749dbdf850319d0df8b1602
sadrabn48 <106390330+sadrabn48@users.noreply.github.com>
2022-09-20 09:23:23 -0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,54 @@
+
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "6ix9ine - Fefe",
+    artist: "6ix9ine",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    
+    name: " sia | zayn - dusk till dawn",
+    artist: " sia  zayn",
+    img: "music-8",
+    src: "music-8"
+  }
+
+];
